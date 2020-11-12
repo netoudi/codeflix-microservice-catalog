9b5dfcf54f3fe8ca25bf93cee8a0d5be366ffe0c
netoudi <netoudi@rocketmail.com>
2020-11-12 14:00:00 -0300

    Conectando a aplicação com Exchanges e Queues

--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -1,6 +1,8 @@
 import { Context } from '@loopback/context';
 import { Server } from '@loopback/core';
-import { connect, Connection } from 'amqplib';
+import { Channel, connect, Connection, Replies } from 'amqplib';
+import AssertQueue = Replies.AssertQueue;
+import AssertExchange = Replies.AssertExchange;
 
 export class RabbitmqServer extends Context implements Server {
   private _listening: boolean;
@@ -14,6 +16,35 @@ export class RabbitmqServer extends Context implements Server {
     });
 
     this._listening = true;
+    this.boot();
+  }
+
+  async boot(): Promise<void> {
+    const channel: Channel = await this.conn.createChannel();
+    const queue: AssertQueue = await channel.assertQueue('first-queue');
+    const exchange: AssertExchange = await channel.assertExchange(
+      'amq.direct',
+      'direct',
+    );
+
+    await channel.bindQueue(queue.queue, exchange.exchange, 'my-routing-key');
+
+    const result = channel.sendToQueue(
+      'first-queue',
+      Buffer.from('Hello world!'),
+    );
+
+    channel.publish(
+      'amq.direct',
+      'my-routing-key',
+      Buffer.from('Published by routing key...'),
+    );
+
+    channel.consume(queue.queue, (message) => {
+      console.log(message?.content.toString());
+    });
+
+    console.log(result);
   }
 
   async stop(): Promise<void> {
